0c7109130efc854a0640bb59e4a80231bc0b5079
msanjith130@gmail.com <148097204+sanjithhithub@users.noreply.github.com>
2024-03-11 07:27:12 +0530

    first program

---
+++ variable.py
@@ -0,0 +1,13 @@
+name = "RAM"
+print(name)
+
+name ="sam"
+print(name)
+
+price = 10.0
+print(type(price))
+print(price)
+print("------------------------------------------")
+number = 10
+print(number)
+print(type(number))
